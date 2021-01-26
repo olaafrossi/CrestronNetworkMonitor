5d08c62345ebd9452e7a57159ee107880fd2910d
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-01-26 17:18:28 +0700

    work around
    the issue is a limitation of the DI builder, it won't accept a file path out of it's hosting context. waiting on a fix from smarter people than me

---
+++ ConsoleJsonParse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace ConsoleJsonParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string jsonString = File.ReadAllText(@"C:\ThreeByteIntermedia\CrestronNetworkMonitor\Settings\appsettings.json");
+            Console.WriteLine(jsonString);
+
+            //AppSettings app = new AppSettings();
+
+            using (StreamReader r = new StreamReader(@"C:\ThreeByteIntermedia\CrestronNetworkMonitor\Settings\appsettings.json"))
+            {
+                string json = r.ReadToEnd();
+                AppSettings app = JsonConvert.DeserializeObject<AppSettings>(json);
+                Console.WriteLine("here we go");
+                Console.WriteLine(app.UdpPort.ToString());
+                app.UdpPort++;
+                Console.WriteLine(app.UdpPort.ToString());
+            }
+
+            
+
+
+            //weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+
+        }
+    }
+}
